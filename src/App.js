ceac7dff30f4d97b9aa65f9d9cc78ce0837ab441
Mark Apinis <mapinis2019@gmail.com>
2017-11-03 11:55:24 -0400

    React stuffs and buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// const Header = ({text}) => <h1>{text.length}</h1>
+
+class Button extends Component {
+  constructor(props){
+    super(props);
+    this.state = {buttonText: props.buttonText, clickedText: props.clickedText, clicked: false};
+  }
+
+  click(e) {
+    this.setState(s => ({clicked: !s.clicked}))
+  }
+
   render() {
     return (
+      <button onClick = {this.click.bind(this)} className={this.state.clicked ? "clicked": "unclicked"}>
+        {this.state.clicked ? this.state.clickedText : this.state.buttonText}
+      </button>
+    );
+  }
+
+}
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {counter:1};
+  }
+
+  onIncrement(e){
+    this.setState(s => ({counter: s.counter + 1}))
+  }
+
+  onDecrement(e){
+    this.setState(s => ({counter: s.counter - 1}))
+  }
+
+  onReset(e){
+    this.setState({counter:1});
+  }
+
+  render() {
+    // const headers = ["This", "Is", "A", "Test"]
+
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.onIncrement.bind(this)}>+</button>
+        <button onClick={this.onDecrement.bind(this)}>-</button>
+        <button onClick={() => this.onReset()}>1</button> */}
+        <Button buttonText="ready" clickedText="waiting..." />
+        <Button buttonText="click me" clickedText="clicked" />
       </div>
     );
   }
